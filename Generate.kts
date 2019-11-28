463606c278889ee660ff9e8ba50fa3d621e77ff0
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2019-11-28 20:18:28 +0300

    v.1.3.61

--- Generate.kts
+++ Generate.kts
@@ -1,7 +1,7 @@
 #! /bin/env kscript
 
-@file:DependsOn("org.jetbrains.kotlin:kotlin-compiler:1.3.60")
-val version = "1.3.60"
+@file:DependsOn("org.jetbrains.kotlin:kotlin-compiler:1.3.61")
+val version = "1.3.61"
 
 val warnings =
         org.jetbrains.kotlin.diagnostics.Errors::class.java.declaredFields
