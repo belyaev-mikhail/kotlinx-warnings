8e1aafc031dd2d207df68c00940139b5b71b825a
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2020-10-01 14:18:37 +0300

    Fix schedule task

--- current.sh
+++ current.sh
@@ -3,7 +3,7 @@ echo "Latest Kotlin version is " $LATEST_KOTLIN_VERSION
 CURRENT_VERSION=$(curl -sS https://dl.bintray.com/vorpal-research/kotlin-maven/ru/spbstu/kotlinx-warnings/maven-metadata.xml | grep -o '<latest>[^<]*</latest>' | grep -o "[0-9.]*")
 echo "Latest kotlinx-warnings version is " $CURRENT_VERSION
 if [ "$LATEST_KOTLIN_VERSION" != "$CURRENT_VERSION" ]; then
-    ./gradlew -PkotlinVersion=LATEST_KOTLIN_VERSION publish
+    ./gradlew -PkotlinVersion="$LATEST_KOTLIN_VERSION" publish
 else
     echo "We are up-to-date with current Kotlin version"
 fi
