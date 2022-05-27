a31f07c71f3fb8be97542c4b4018963143e9a268
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2022-05-27 14:51:51 +0300

    change read token

--- current.sh
+++ current.sh
@@ -2,10 +2,10 @@ LATEST_KOTLIN_VERSION=$(curl -sS https://repo1.maven.org/maven2/org/jetbrains/ko
 echo "Latest Kotlin version is " $LATEST_KOTLIN_VERSION
 # password is a public readonly github token, so we share it deliberately
 GHP_USER=vorpal-reseacher
-GHP_TOKEN=$(echo -e '\x31\x30\x62\x37\x64\x66\x31\x32\x63\x64')
-GHP_TOKEN+=$(echo -e '\x35\x34\x38\x37\x34\x65\x30\x34\x35\x35')
-GHP_TOKEN+=$(echo -e '\x38\x31\x63\x39\x39\x62\x31\x66\x32\x30')
-GHP_TOKEN+=$(echo -e '\x38\x65\x31\x61\x35\x33\x65\x36\x32\x38')
+GHP_TOKEN+=$(echo -e '\x67\x68\x70\x5f\x46\x4a\x38\x67\x70\x67')
+GHP_TOKEN+=$(echo -e '\x78\x49\x63\x33\x75\x78\x54\x32\x7a\x39')
+GHP_TOKEN+=$(echo -e '\x6b\x4d\x61\x62\x4e\x58\x67\x33\x79\x72')
+GHP_TOKEN+=$(echo -e '\x4c\x4a\x4f\x48\x32\x4f\x62\x64\x6a\x6c')
 CURRENT_VERSION=$(curl -u $GHP_USER:$GHP_TOKEN -sS https://maven.pkg.github.com/vorpal-research/kotlin-maven/ru/spbstu/kotlinx-warnings/maven-metadata.xml | grep -oP '(?<=<latest>)[^<]*(?=</latest>)')
 echo "Latest kotlinx-warnings version is" $CURRENT_VERSION
 if [ "$LATEST_KOTLIN_VERSION" != "$CURRENT_VERSION" ]; then
